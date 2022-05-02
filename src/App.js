7265b3b252006243b3e52497a2014be4adc590a2
xaviguasch <xaviguasch@gmail.com>
2022-05-02 10:55:28 +0200

    added Navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 
+import Navigation from './components/Navigation'
 import Main from './components/Main'
 
 function App() {
   return (
     <div className='App'>
       <h1>Intro Section</h1>
+      <Navigation />
 
       <Main />
     </div>
