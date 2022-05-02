0864bdd4c4a2993901a46a86cf31f16b1faa1342
xaviguasch <xaviguasch@gmail.com>
2022-05-02 10:50:20 +0200

    added Main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
 
+import Main from './components/Main'
+
 function App() {
   return (
     <div className='App'>
       <h1>Intro Section</h1>
+
+      <Main />
     </div>
   )
 }
